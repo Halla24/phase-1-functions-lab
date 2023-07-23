8352010d515172cec458862a394ed01e6b203dbe
halla <aniabisso.16@gmail.com>
2023-07-23 22:16:01 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hqLocation = 42;
+    return Math.abs(someValue - hqLocation);
+  }
+
+
+  function distanceFromHqInFeet(someValue) {
+    const blocks = distanceFromHqInBlocks(someValue);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+  }
+
+
+  function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(destination - start);
+    const feetPerBlock = 264;
+    return blocks * feetPerBlock;
+  }
+
+
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let fare;
+
+    if (distance <= 400) {
+      fare = 0;
+    } else if (distance <= 2000) {
+      fare = 0.02 * (distance - 400);
+    } else if (distance <= 2500) {
+      fare = 25;
+    } else {
+      return 'cannot travel that far';
+    }
+
+    return fare;
+  }
+
+  module.exports = {
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice,
+  };
